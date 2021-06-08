857469c66da95e644593deda196fd4aa5e4adb72
Ahmad <arnaufal96@gmail.com>
2021-06-08 11:17:55 +0700

    editing home html, css, script

--- Frawlsalla/Script/Script.js
+++ Frawlsalla/Script/Script.js
@@ -1,21 +1,3 @@
-<script>
-    var Slideidx = 1;
-    displaySlide(Slideidx);
-
-    function moveSlide(x){
-        displaySlide(Slideidx + x);
-    }
-
-    function activeSlide(x){
-        displaySlide(Slideidx = x);
-    }
-
-    function displaySlide(x){
-        var i;
-        var totalSlides = document.getElementsbyClassName("slide");
-
-        var dots = document.getElementsbyClassName("footerdot");
-
-        if (x > totalSlides.length; i++)
-    }
-</script>
+$(document).ready(function(){
+    $('sliders').slick();
+  });
